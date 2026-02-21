c1b10ebce5d2e7b909a962ab78a5f5cebe91082a
univied <amerfoe@gmail.com>
2026-02-21 22:25:21 +0300

    fix: rename FluentTranslator to Translator

--- src/fluent_telegrinder/sources.py
+++ src/fluent_telegrinder/sources.py
@@ -1,21 +1,18 @@
 from nodnod import scalar_node
 from telegrinder.node import UserSource
 
-from fluent_telegrinder.i18n import FluentTranslator
+from fluent_telegrinder.i18n import Translator
 
 
 @scalar_node
 class DefaultLocaleSource:
     @classmethod
     async def compose(cls) -> str:
-        return FluentTranslator.config.default_locale
+        return Translator.config.default_locale
 
 
 @scalar_node
 class UserLanguageSource:
     @classmethod
     async def compose(cls, user: UserSource) -> str:
-        return (
-            user.language_code
-            .unwrap_or(FluentTranslator.config.default_locale)
-        )
+        return user.language_code.unwrap_or(Translator.config.default_locale)
