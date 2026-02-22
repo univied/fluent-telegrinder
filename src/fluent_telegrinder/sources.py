1fc6da8168087bcb9b91e9d376bd3c24d3d5b2fc
univied <amerfoe@gmail.com>
2026-02-22 11:22:03 +0300

    fix: source nodes

--- src/fluent_telegrinder/sources.py
+++ src/fluent_telegrinder/sources.py
@@ -7,12 +7,12 @@ from fluent_telegrinder.i18n import Translator
 @scalar_node
 class DefaultLocaleSource:
     @classmethod
-    async def compose(cls) -> str:
+    async def __compose__(cls) -> str:
         return Translator.config.default_locale
 
 
 @scalar_node
 class UserLanguageSource:
     @classmethod
-    async def compose(cls, user: UserSource) -> str:
+    async def __compose__(cls, user: UserSource) -> str:
         return user.language_code.unwrap_or(Translator.config.default_locale)
